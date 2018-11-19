874b2217df4a13a36ee769aaf1ec3a41712a05aa
Dominic Jefferson <Dominic.jefferson@cooop.co.uk>
2018-11-19 09:30:08 +0000

    tings changed
    adqwcafad

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import blogPost
 from .forms import PostForm
-from django.shortcuts import redirect ,render ,get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
 
 def post_list(request):
     posts = blogPost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+@login_required
 def post_new(request):
-
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -21,9 +23,11 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
-def post_detail(request,pk ):
-    post = get_object_or_404(blogPost , pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
+def post_detail(request, pk):
+    post = get_object_or_404(blogPost, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
--- website/urls.py
+++ website/urls.py
@@ -22,8 +22,9 @@ from django.urls import path
 """
 from django.urls import path, include
 from django.contrib import admin
-
+from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView, name='login'),
     path('', include('blog.urls')),
 ]
